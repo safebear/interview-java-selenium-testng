822b32b524b8d05601e04f61b47505dc484148c7
Simon Stratton <simon.stratton@safebear.co.uk>
2019-04-02 21:06:45 +0100

    Setting launchers for selenium server to run in the background

--- src/test/resources/selenium_server/launch-chrome-node.sh
+++ src/test/resources/selenium_server/launch-chrome-node.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -Dwebdriver.chrome.driver='./chromedriver' -jar selenium-server-standalone-3.141.59.jar -role webdriver -hub 'http://localhost:4444/grid/register' -browser 'browserName = chrome, platform = MAC' -port 5556
+java -Dwebdriver.chrome.driver='./chromedriver' -jar selenium-server-standalone-3.141.59.jar -role webdriver -hub 'http://localhost:4444/grid/register' -browser 'browserName = chrome, platform = MAC' -port 5556 &
--- src/test/resources/selenium_server/launch-firefox-node.sh
+++ src/test/resources/selenium_server/launch-firefox-node.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/grid/register  -browser 'browserName = firefox' -port 5555
+java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/grid/register  -browser 'browserName = firefox' -port 5555 &
